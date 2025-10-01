14ae5441f99478f0bf9464657442bcce7cce74bb
Ricardo R-S <rodriguez@schrodinger.com>
2025-10-01 11:21:27 +0200

    disable dll import/export for windows static builds

--- chemdraw/CoreChemistryAPI.h
+++ chemdraw/CoreChemistryAPI.h
@@ -14,7 +14,9 @@
 # endif
 #endif
 #if defined(_WIN32) || defined(__CYGWIN__)
-  #ifdef CHEMDRAW_BUILD
+  #if defined(CHEMDRAW_STATIC_LINK)
+    #define CORE_CHEMISTRY_API
+  #elif defined(CHEMDRAW_BUILD)
     // building the DLL
     #define CORE_CHEMISTRY_API __declspec(dllexport)
   #else
